bb959d7425e89994e04ee72ef433cb74393f79fb
Rohith20062509 <sunny20062509@gmail.com>
2026-02-24 15:36:21 +0530

    uc1 commit

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,9 @@
+import javax.rmi.ssl.SslRMIClientSocketFactory;
+
+public class PalindromeCheckerApp {
+    static void main() {
+        System.out.println("Welcome to PalindromeCheckerApp");
+        System.out.println("Version 1.0");
+        System.out.println("System Initialised Successfully");
+    }
+}
