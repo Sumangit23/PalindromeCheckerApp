273fd4ee9601991a8a7d65ef1def3415449a212c
Rohith20062509 <sunny20062509@gmail.com>
2026-02-24 16:14:00 +0530

    uc2 commit

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,8 +2,26 @@ import javax.rmi.ssl.SslRMIClientSocketFactory;
 
 public class PalindromeCheckerApp {
     static void main() {
-        System.out.println("Welcome to PalindromeCheckerApp");
-        System.out.println("Version 1.0");
-        System.out.println("System Initialised Successfully");
+                // Hardcoded string
+                String input = "madam";
+                boolean isPalindrome = true;
+
+                // Loop only till half of the string length
+                for (int i = 0; i < input.length() / 2; i++) {
+                    if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+
+                // Print result
+                if (isPalindrome) {
+                    System.out.println(input + " is a Palindrome");
+                } else {
+                    System.out.println(input + " is NOT a Palindrome");
+                }
+            }
+        }
+
     }
 }
